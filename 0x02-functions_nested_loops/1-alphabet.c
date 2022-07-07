5e00b8696a3d8a53ac294178697681d6ab42706e
DivineAsim <dareedrichards1@gmail.com>
2022-07-07 04:29:50 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,22 @@
-ii
+#include "main.h"
+
+/**
+ * main: - prints_alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always (0)
+ */
+
+main(void)
+{
+
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+
+return;
+}
