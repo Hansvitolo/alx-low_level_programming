25f56d51ac2437cade277e8524741798d0cf8330
DivineAsim <dareedrichards1@gmail.com>
2022-07-14 12:10:25 -0700

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,22 @@
-22
+#include "main.h"
+
+/**
+ * _strlen: function that returns the length of a string
+ * @s: string to count
+ *
+ * Return: length of a script
+ */
+
+int _strlen(char *s)
+{
+
+int count = 0;
+while (*s != '\0')
+{
+count++;
+s++;
+}
+
+return (count);
+
+}
