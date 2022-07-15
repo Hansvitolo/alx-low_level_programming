6cf86a015054ffe6e95cfafcd13183c95881ab0b
DivineAsim <dareedrichards1@gmail.com>
2022-07-15 08:48:06 -0700

    arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,38 @@
 #include "main.h"
 
 /**
- * puts2 - prints one char out of 2 of a string.
- * @str: input string.
+ * cap_string - capitalizes all words of a string
+ * @s: input string
  *
- * Return: no return.
+ * Return: the pointer to string
  */
 
-void puts2(char *str)
+char *cap_string(char *s)
 
 {
 
-int count = 0;
+int count = 0, i;
 
-while (count >= 0)
+int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
+
 {
-if (str[count] == '\0')
+for (i = 0; i < 13; i++)
 {
-_putchar('\n');break;																		}
-if (count % 2 == 0)
-_putchar(str[count]);								count++;
+if (*(s + count) == sep_words[i])
+{
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
 }
+}
+count++;
+}
+
+return (s);
 }
