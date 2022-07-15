52d187d999a141075b8edd2cf778299398874fc6
DivineAsim <dareedrichards1@gmail.com>
2022-07-15 07:28:39 -0700

    pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,10 +16,13 @@ int count = 0;
 while (count >= 0)
 {
 if (str[count] == '\0')
-										{
+{
+
 _putchar('\n');
-break;										}
+break;
+										}
 _putchar(str[count]);
 count++;
-}
+										}
+
 }
