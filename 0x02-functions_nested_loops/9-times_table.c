459340b8418f6627dbc8e7a40e49d2dc99394f26
DivineAsim <dareedrichards1@gmail.com>
2022-07-15 06:38:14 -0700

    nested

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,6 @@
  */
 
 void times_table(void)
-
 {
 
 int a, b, op;
@@ -20,12 +19,12 @@ for (b = 1; b <= 9; b++)
 op = a * b;
 _putchar(44);
 _putchar (32);
+
 if (op <= 9)
 {
 _putchar (32);
 _putchar (op + 48);
 }
-{
 else
 {
 _putchar ((op / 10) + 48);
