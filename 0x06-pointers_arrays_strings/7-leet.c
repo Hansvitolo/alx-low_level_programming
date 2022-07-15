257dd9b2379b4de1202254a7346c2918ca34eda8
DivineAsim <dareedrichards1@gmail.com>
2022-07-15 08:56:39 -0700

    arrays

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,7 +4,7 @@
  * leet - encodes a string into 1337
  * @s: input string.
  *
- * Return: the pointer to dest.
+ * Return: the string pointer
  */
 
 char *leet(char *s)
@@ -22,13 +22,20 @@ int numbers[] = {52, 51, 48, 55, 49};
 while (*(s + count) != '\0')
 {
 for (i = 0; i < 5; i++)
-if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+
 {
+
+if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+
+{
+
 *(s + count) = numbers[i];
+
 break;
 }
 }
 count++;
 }
-return ('s');
+
+return (s);
 }
