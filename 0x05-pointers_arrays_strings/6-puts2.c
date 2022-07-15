b2294996dfc32e8463cf3737a5bd54b1bee62ce6
DivineAsim <dareedrichards1@gmail.com>
2022-07-14 19:11:20 -0700

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,19 @@
-six
+#include "main.h"
+
+/**
+ * puts2 - function for printing other characters from first
+ * @str: function for printing
+ *
+ */
+
+void puts2(char *str)
+
+{
+
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
